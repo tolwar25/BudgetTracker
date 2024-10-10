d1e3564c992fd1905e9ae1040183f476a6892a43
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-10 23:55:19 +0400

    Получить html страницу в java коде

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,25 @@
 package org.example;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Хеллоу Ворлд");
-        Student s1 = new Student("Petya");
-        Student s2 = new Student("Sanya");
-        Student.study();
-        System.out.println(s1.greeting());
-        System.out.println(s2.greeting());
-    }
+        try {
+            // Отправляем GET-запрос к веб-странице
+            Document doc = Jsoup.connect("https://magnit.ru/catalog?shopCode=992301")
+                    .userAgent("Mozilla/5.0")  // Задаем User-Agent (может помочь при блокировке ботов)
+                    .timeout(5000)  // Устанавливаем тайм-аут соединения
+                    .get();  // Выполняем GET-запрос
 
+            // Выводим содержимое страницы в консоль
+            System.out.println(doc.html());  // Выводим HTML-код страницы
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
