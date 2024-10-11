7778758c09e66b9f4554d98f97d81a5d1e1c3365
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-11 18:29:16 +0400

    Записать html страницу в файл

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,18 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static String MAGNIT_URL = "https://magnit.ru/catalog?shopCode=";
+    static String SHOP_ID = "992301";
+
     public static void main(String[] args) {
         try {
-            // Отправляем GET-запрос к веб-странице
-            Document doc = Jsoup.connect("https://magnit.ru/catalog?shopCode=992301")
-                    .userAgent("Mozilla/5.0")  // Задаем User-Agent (может помочь при блокировке ботов)
-                    .timeout(5000)  // Устанавливаем тайм-аут соединения
-                    .get();  // Выполняем GET-запрос
-
-            // Выводим содержимое страницы в консоль
-            System.out.println(doc.html());  // Выводим HTML-код страницы
-
+            Document doc = Jsoup.connect(MAGNIT_URL + SHOP_ID)
+                    .get();
+            System.out.println(doc.html());
         } catch (IOException e) {
             e.printStackTrace();
         }
