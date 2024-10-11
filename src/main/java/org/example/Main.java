29df7f6e0dba3250c6b7223a0bc61fab5795ba53
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-11 19:09:40 +0400

    Записать html страницу в файл

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
+    static String MAGNIT_URL = "https://magnit.ru/catalog?shopCode=";
+    static String SHOP_ID = "992301";
+
     public static void main(String[] args) {
         try {
-            // Отправляем GET-запрос и получаем HTML
-            Document doc = Jsoup.connect("https://magnit.ru/catalog?shopCode=992301")
-                    .userAgent("Mozilla/5.0")
-                    .timeout(5000)
+            Document doc = Jsoup.connect(MAGNIT_URL + SHOP_ID)
                     .get();
-
-            // Записываем содержимое в файл с помощью NIO (Files.write)
             Files.write(Paths.get("output.html"), doc.html().getBytes());
-
             System.out.println("HTML содержимое успешно записано в файл");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
