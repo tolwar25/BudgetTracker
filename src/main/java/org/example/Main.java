cbda93af451474e9cbfadd353f78098d32064ecc
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-13 17:31:57 +0400

    Вытащить тэги карточек товаров в java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main {
     static String MAGNIT_URL = "https://magnit.ru/catalog?shopCode=";
@@ -18,7 +16,7 @@ public class Main {
             Document doc = Jsoup.connect(MAGNIT_URL + SHOP_ID).get();
             Elements titles = doc.select("article");
             for (Element title: titles){
-                String price = title.select(".unit-catalog-product-preview-prices__top-row").text();
+                String price = title.select(".unit-catalog-product-preview-prices__regular").text();
                 String name = title.select(".unit-catalog-product-preview-title").text();
                 System.out.println(price + " " + name);
             }
