d0d801f7f38214331c7e00a51b8580dae55b2645
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-17 22:53:18 +0400

    BT-8 Вытащить тэги карточек товаров в java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 public class Main {
     static String MAGNIT_URL = "https://magnit.ru/catalog?shopCode=";
@@ -15,10 +17,16 @@ public class Main {
         try {
             Document doc = Jsoup.connect(MAGNIT_URL + SHOP_ID).get();
             Elements titles = doc.select("article");
+            FileWriter writer = new FileWriter("output.txt");
             for (Element title: titles){
                 String price = title.select(".unit-catalog-product-preview-prices__regular").text();
+                String numericPrice = price.replaceAll("[^\\d.,]", "").replace(",", ".");
+                BigDecimal bigDecimalPrice = new BigDecimal(numericPrice);
+
                 String name = title.select(".unit-catalog-product-preview-title").text();
-                System.out.println(price + " " + name);
+
+                String pricePerQuantity = title.select(".unit-catalog-product-preview-unit-value").text();
+                writer.write(bigDecimalPrice + " " + name + " " +pricePerQuantity + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
