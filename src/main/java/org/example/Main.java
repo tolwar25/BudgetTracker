444f40f322246cbf01c5abe32793d459efe2412d
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-22 20:05:42 +0400

    BT-9 Создал класс Product

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,14 @@ import org.jsoup.select.Elements;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Main {
     final static String MAGNIT_URL = "https://magnit.ru/catalog?shopCode=";
     final static String SHOP_ID = "992301";
     final static String OUTPUT = "output.txt";
+
+    static ArrayList<Product> products = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         try (FileWriter writer = new FileWriter(OUTPUT)) {
@@ -28,9 +31,12 @@ public class Main {
                 String name = title.select(".unit-catalog-product-preview-title").text();
 
                 String pricePerQuantity = title.select(".unit-catalog-product-preview-unit-value").text();
-                writer.write(pricePerQuantity.isEmpty() ?
-                        price + " " + name + "\n" :
-                        price + " " + name + " " + pricePerQuantity + "\n");
+
+                products.add(new Product(price, name, pricePerQuantity));
+            }
+
+            for (Product product : products) {
+                System.out.println(product);
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private String name;
+    private String pricePerQuantity;
+    private BigDecimal price;
+
+    public Product(BigDecimal price, String name, String pricePerQuantity) {
+        this.price = price;
+        this.name = name;
+        this.pricePerQuantity = pricePerQuantity;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public String getPricePerQuantity(String pricePerQuantity) {
+        return pricePerQuantity;
+    }
+
+    public BigDecimal getPrice(BigDecimal price) {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return pricePerQuantity.isEmpty() ?
+                price + " " + name + "\n" :
+                price + " " + name + " " + pricePerQuantity + "\n";
+    }
 }
