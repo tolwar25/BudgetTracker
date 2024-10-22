411ce19476cd4fdde5d6a2337286edeec8c3d2da
dasdasdaaw1 <tolwar25@gmail.com>
2024-10-22 20:25:30 +0400

    BT-9 Записал данные из Arraylist в файл

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -13,18 +13,6 @@ public class Product {
         this.pricePerQuantity = pricePerQuantity;
     }
 
-    public String getName(String name) {
-        return name;
-    }
-
-    public String getPricePerQuantity(String pricePerQuantity) {
-        return pricePerQuantity;
-    }
-
-    public BigDecimal getPrice(BigDecimal price) {
-        return price;
-    }
-
     @Override
     public String toString() {
         return pricePerQuantity.isEmpty() ?
